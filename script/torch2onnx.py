f7807c19209e54dde1370700423440579f2fc90a
yoonhero <yoonhero06@naver.com>
2023-08-13 00:35:13 +0900

    feat: OH

--- script/torch2onnx.py
+++ script/torch2onnx.py
@@ -1,12 +1,18 @@
 import torch
 
-def convert(token_size, emb_size=100, dir="../checkpoint/word2vec.pt", out="../checkpoint/word2vec.pt"):
-    emb = torch.nn.Embedding(token_size, emb_size)
-    emb.load_state_dict(torch.load(dir))
-    x = torch.randint(1, 11756, (1, 10))
-    torchout = emb(x)
+def convert(token_size, emb_size=100, dir="../checkpoint/word2vec.pt", out="../checkpoint/word2vec.onnx"):
+    checkpoint = torch.load(dir)
+    for name, param in checkpoint.items():
+        if "emb" in name:
+            emb = torch.nn.Embedding(token_size, emb_size)
+            emb.load_state_dict({"weight": param})
+            x = torch.randint(1, token_size, (1, 10))
+            torchout = emb(x)
 
-    torch.onnx.export(emb, x, out, export_params=True, do_constant_folding=True, input_names=["input"], output_names=["output"])
+            torch.onnx.export(emb, x, out, export_params=True, do_constant_folding=True, input_names=["input"], output_names=["output"], dynamic_axes={"input": {0: "batch", 1: "seq"}})
+            break
 
 if __name__ == "__main__":
-    convert()
+    convert(50257)
+
+    
